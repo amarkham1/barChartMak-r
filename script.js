f6d252f6047cdf82ac3ebad40b5d2a1b1801aba0
amarkham1 <andrewjmarkham1@gmail.com>
2020-08-18 20:28:59 -0400

    added title, x axis, y axis

--- script.js
+++ script.js
@@ -4,18 +4,44 @@ function drawBarChart(data, options, element) {
   const CHARTHEIGHT = (options['height'] ? options['height'] : 400);
   const CHARTWIDTH = (options['width'] ? options['width'] : 600);
   const YAXISTICKS = (options['yAxisTicks'] ? options['yAxisTicks']: 5 );
+  const YAXISMAX = (Math.floor(MAXVALUE / YAXISTICKS) + 1) * YAXISTICKS;
   const TITLEHEIGHT = 40;
   const YAXISWIDTH = 40;
   const XAXISHEIGHT = 40;
-
+  const TITLECOLOUR = (options['titleColour'] ? options['titleColour']: "gray" );
+  const TITLEFONTSIZE = (options['titleFontSize'] ? options['titleFontSize']: "20" );
+  const TITLETEXT = (options['titleText'] ? options['titleText']: 'YOUR CHART TITLE!' );
 
   let chart = $('<div></div>').addClass('chart-container').width(CHARTWIDTH).height(CHARTHEIGHT);
-  let title = $('<div></div>').addClass('title-container').width(CHARTWIDTH).height(TITLEHEIGHT);
-  let yAxis = $('<div></div>').addClass('yAxis-container').width(YAXISWIDTH).height(CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT);
-  let xAxis = $('<div></div>').addClass('xAxis-container').width(CHARTWIDTH).height(XAXISHEIGHT);
+
+  let titleContainer = $('<div></div>').addClass('title-container').width(CHARTWIDTH).height(TITLEHEIGHT);
+  let titleText = $('<div>' + TITLETEXT + '</div>').addClass('title-text').css({
+    fontSize: TITLEFONTSIZE + 'px',
+    color: TITLECOLOUR
+  });
+  let title = titleContainer.append(titleText);
   chart.append(title);
-  chart.append(yAxis);
-  chart.append(xAxis);
+
+  let yAxisContainer = $('<div></div>').addClass('yAxis-container').width(YAXISWIDTH).height(CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT);
+  for(i = 0; i < YAXISTICKS; i++) {
+    let yAxisTick = $('<div>' + (YAXISMAX - (i * YAXISMAX / YAXISTICKS)) + '</div>').addClass('yAxisTick').width(YAXISWIDTH).height((CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT) / YAXISTICKS);
+    yAxisContainer.append(yAxisTick);
+  }
+  chart.append(yAxisContainer);
+
+  let dataBox = $('<div></div>').addClass('data-container').width(CHARTWIDTH - YAXISWIDTH).height(CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT);
+  chart.append(dataBox);
+
+  let xAxisContainer = $('<div></div>').addClass('xAxis-container').width(CHARTWIDTH).height(XAXISHEIGHT);
+  xAxisLeftMargin = $('<div></div>').width(YAXISWIDTH).height(XAXISHEIGHT);
+  xAxisContainer.append(xAxisLeftMargin);
+  for(i = 0; i < DATALEN; i++) {
+    let xAxisTick = $('<div>' + data[i] + '</div>').addClass('xAxisTick').width((CHARTWIDTH - YAXISWIDTH) / DATALEN).height(XAXISHEIGHT);
+    xAxisContainer.append(xAxisTick);
+  }
+  chart.append(xAxisContainer);
+
+
   return $("body").append(chart);
 }
 
@@ -32,3 +58,4 @@ function findMax(data) {
   }
   return max;
 }
+
