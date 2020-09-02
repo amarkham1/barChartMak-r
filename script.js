0c402a3b1d82b04a6c32d1cf81992dfa1424bfe1
amarkham1 <andrewjmarkham1@gmail.com>
2020-09-01 20:28:31 -0400

    added stacked bar

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ function drawBarChart(data, options, element) {
   const TITLECOLOUR = (options['titleColour'] ? options['titleColour'] : "black" );
   const TITLEFONTSIZE = (options['titleFontSize'] ? options['titleFontSize'] : "20" );
   const TITLETEXT = (options['titleText'] ? options['titleText'] : 'YOUR CHART TITLE!' );
+  const BARCOLOUR = (options['barColour'] ? options['barColour'] : ['darkgreen']);
+  const BARLABELPOSITION = (options['barLabelPosition'] ? options['barLabelPosition'] : 'center');
   const YAXISMAX = (Math.floor(MAXVALUE / YAXISTICKS) + 1) * YAXISTICKS;
   const TITLEHEIGHT = 30;
   const YAXISWIDTH = 30;
   const XAXISHEIGHT = 30;
   const XAXISWIDTH = CHARTWIDTH - YAXISWIDTH;
-  const BARPADDING = 0.04 * (CHARTWIDTH - YAXISWIDTH) * 5 / DATALEN;
+  const BARSPACING = (options['barSpacing'] ? options['barSpacing'] : 0.04 * (CHARTWIDTH - YAXISWIDTH) * 5 / DATALEN);
+  const STACKEDDATATOTALS = stackedDataTotals(data);
 
   let chart = $('<div></div>').addClass('chart-container').width(CHARTWIDTH).height(CHARTHEIGHT);
 
@@ -45,11 +48,38 @@ function drawBarChart(data, options, element) {
     'background-size':'100% ' + ((CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT) / YAXISTICKS) + 'px'
   });
   for(i = 0; i < DATALEN; i++) {
-    let leftPadding = $('<div></div>').addClass('dataPadding').width(BARPADDING).height((CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT));
-    let dataBar = $('<div>' + data[i] + '</div>').addClass('dataBar').width((XAXISWIDTH - DATALEN * BARPADDING * 2) / DATALEN).height((data[i] / YAXISMAX) * (CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT));
-    let rightPadding = $('<div></div>').addClass('dataPadding').width(BARPADDING).height((CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT));
+    let leftPadding = $('<div></div>').addClass('dataPadding').width(BARSPACING).height((CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT));
+    let dataBarContainer = $('<div></div>').addClass('dataBarContainer').width((XAXISWIDTH - DATALEN * BARSPACING * 2) / DATALEN).height((STACKEDDATATOTALS[i] / YAXISMAX) * (CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT));
+    if (!data[i].length) {
+      let dataBar = $('<div>' + data[i] + '</div>').addClass('dataBar').width((XAXISWIDTH - DATALEN * BARSPACING * 2) / DATALEN).height((data[i] / YAXISMAX) * (CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT)).css({
+        'align-items': BARLABELPOSITION
+      });
+      if (BARCOLOUR.length === 1) {
+        dataBar.css({'background-color': BARCOLOUR})
+      } else {
+        dataBar.css({'background-color': BARCOLOUR[i]})
+      }
+      dataBarContainer.append(dataBar);
+    } else {
+      for (j = 0; j < data[i].length; j++) {
+        let dataBar = $('<div></div>').addClass('dataBar').width((XAXISWIDTH - DATALEN * BARSPACING * 2) / DATALEN).height((data[i][j] / YAXISMAX) * (CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT)).css({
+          'align-items': BARLABELPOSITION
+        });
+        if (data[i][j] !== 0) {
+          dataBar.append(data[i][j]);
+        }
+        if (BARCOLOUR.length === 1) {
+          dataBar.css({'background-color': BARCOLOUR ? BARCOLOUR : 'darkgreen'})
+        } else {
+          dataBar.css({'background-color': BARCOLOUR[i][j] ? BARCOLOUR[i][j] : 'darkgreen'})
+        }
+        dataBarContainer.append(dataBar);
+      }
+    }
+
+    let rightPadding = $('<div></div>').addClass('dataPadding').width(BARSPACING).height((CHARTHEIGHT - TITLEHEIGHT - XAXISHEIGHT));
     dataBox.append(leftPadding);
-    dataBox.append(dataBar);
+    dataBox.append(dataBarContainer);
     dataBox.append(rightPadding);
   }
   chart.append(dataBox);
@@ -73,11 +103,37 @@ function findMax(data) {
   }
 
   let max = Number.MIN_SAFE_INTEGER;
+  let currentTotal = 0;
   for (let i = 0; i < data.length; i++) {
-    if (data[i] > max) {
-      max = data[i];
+    if (!data[i].length) {
+      currentTotal += data[i];
+    } else {
+      for (let j = 0; j < data[i].length; j++) {
+        currentTotal += data[i][j];
+      }
     }
+    if (currentTotal > max) {
+      max = currentTotal;
+    }
+    currentTotal = 0;
   }
   return max;
+}
+
+function stackedDataTotals(data) {
+  let stackedDataTotal = [];
+  let currentTotal = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i].length) {
+      currentTotal += data[i];
+    } else {
+      for (let j = 0; j < data[i].length; j++) {
+        currentTotal += data[i][j];
+      }
+    }
+    stackedDataTotal.push(currentTotal);
+    currentTotal = 0;
+  }
+  return stackedDataTotal;
 }
 
